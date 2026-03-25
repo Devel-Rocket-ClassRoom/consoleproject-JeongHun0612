5b0c6c8645694036b35e230cfafa381ca79c4502
송정훈 <sjh960612@naver.com>
2026-03-25 10:51:43 +0900

    프로젝트 템플릿 변경
    콘솔 앱(.NETFramework) -> 콘솔 앱

---
+++ DungeonGame/ScreenManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonGame
+{
+    internal class ScreenManager
+    {
+        char[,] _map;
+
+        public void Initialize()
+        {
+            _map = new char[100, 100];
+        }
+
+        public void Render()
+        {
+            int row = _map.GetLength(0);
+            int col = _map.GetLength(1);
+
+            for (int r = 0; r < row; r++)
+            {
+                for (int c = 0; c < col; c++)
+                {
+                    Console.Write(_map[r, c]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
