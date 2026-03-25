86f45e53e0d0769a6a6922badd9966f45183ed22
송정훈 <sjh960612@naver.com>
2026-03-25 15:45:12 +0900

    DataManager 추가
    Json 기반 데이터를 저장 및 로드 하는 기능의 Manager 추가

---
+++ DungeonGame/DataManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DungeonGame
+{
+    internal static class DataManager
+    {
+        public static string FolderPath = "./GameData";
+
+        public static void SaveData<T>(T data, string fileName)
+        {
+            string filePath = GetFilePath(fileName);
+
+            try
+            {
+                if (!Directory.Exists(FolderPath))
+                    Directory.CreateDirectory(FolderPath);
+
+                string json = JsonSerializer.Serialize<T>(data, new JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText(filePath, json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public static T LoadData<T>(string fileName)
+        {
+            string filePath = GetFilePath(fileName);
+
+            T? data = default;
+
+            try
+            {
+                string json = File.ReadAllText(filePath);
+                data = JsonSerializer.Deserialize<T>(json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return data;
+        }
+
+        private static string GetFilePath(string fileName)
+        {
+            return Path.Combine(FolderPath, $"{fileName}.json");
+        }
+    }
+}
